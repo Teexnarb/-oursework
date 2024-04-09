61353099a76229ed08e155d1981ce1f418b31c40
User <danilgaybeshev@mail.ru>
2024-04-09 13:27:31 +0500

    Initial commit

---
+++ src/main/java/pro/sky/JavaCoursework/JavaCourseworkApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.JavaCoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCourseworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCourseworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/JavaCoursework/JavaCourseworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.JavaCoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCourseworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
